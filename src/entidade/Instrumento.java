2c53a69e55d1be730d4696ba38d964b1e27d8698
Igor Nascimento <nigor6803@gmail.com>
2024-04-24 17:53:34 -0300

    Instrumento completo

---
+++ src/entidade/Instrumento.java
@@ -0,0 +1,20 @@
+package entidade;
+
+public class Instrumento {
+	private String nomeDoInstrumento;
+	
+	public Instrumento() {
+		}
+	
+	public Instrumento(String nomeDoInstrumento) {
+		this.nomeDoInstrumento = nomeDoInstrumento;
+	}
+	
+	public String getNomeDoInstrumento() {
+		return nomeDoInstrumento;
+	}
+	
+	public void setNomeDoInstrumento(String nomeDoInstrumento) {
+		this.nomeDoInstrumento = nomeDoInstrumento;
+	}
+}
