524a7d854bc97cdf392ab30c2bfe889922573c7c
Igor Nascimento <nigor6803@gmail.com>
2024-04-24 17:31:09 -0300

    hora por tocada completo

---
+++ src/entidade/HoraPorTocada.java
@@ -0,0 +1,47 @@
+package entidade;
+
+import java.util.Date;
+
+public class HoraPorTocada {
+	
+	private Date diaDaApresentacao;
+	private Double valorPorHora;
+	private Integer horas;
+	
+	public HoraPorTocada() {
+		}
+	
+	public HoraPorTocada(Date diaDaApresentacao, Double valorPorHora, Integer horas) {
+		this.diaDaApresentacao = diaDaApresentacao;
+		this.valorPorHora = valorPorHora;
+		this.horas = horas;
+	}
+	
+	public Date getDiadoAniversario() {
+		return diaDaApresentacao;
+	}
+	
+	public void setDiaDoAniversario(Date diaDoAniversario) {
+		this.diaDaApresentacao = diaDoAniversario;
+	}
+	
+	public Double getValorPorHora() {
+		return valorPorHora;
+	}
+	
+	public void setValorPorHora(Double valorPorHora) {
+		this.valorPorHora = valorPorHora;
+	}
+	
+	public Integer getHoras() {
+		return horas;
+	}
+	
+	public void setHoras(Integer horas) {
+		this.horas = horas;
+	}
+	
+	public double valorTotal() {
+		return valorPorHora*horas;
+	}
+}
